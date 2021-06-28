c873a779a88163431091f237c8178bce7e7478e8
Gyanendra Kumar <gyan4058@gmail.com>
2021-06-29 03:41:14 +0530

    transition roundoff and loading data from csv and fav setting can be retrieved

--- js/metronome.js
+++ js/metronome.js
@@ -1,3 +1,161 @@
+var strokes_csv = "../assets/csv/strokes.csv";
+var beatcycle_csv = "../assets/csv/beatcycle.csv";
+var strokes ={};
+var dict ={};
+var style = []; 
+var beatCycle = [];
+var beatCyclet = [];
+var tranenabled = [];
+
+    
+
+$.ajax({
+
+    type: 'GET',
+
+    url: strokes_csv,
+
+    dataType: 'text',
+
+    error: function (e) {
+        alert('An error occurred while processing API calls');
+        console.log("Strokes csv call Failed: ", e);
+    },
+
+    success: function (data) {
+        
+        if(data==undefined){
+            console.log(data);
+        }
+
+        var jsonData = $.csv.toObjects(data);
+
+        console.log(jsonData);
+        var b=0;
+
+        $.each(jsonData, function (index, value) {
+            strokes[value['stroke']] = new Audio('assets/audio/'+value['file']);
+            dict[strokes[value['stroke']]] = value['onset'];
+        });
+
+        $.ajax({
+
+            type: 'GET',
+        
+            url: beatcycle_csv,
+        
+            dataType: 'text',
+        
+            error: function (e) {
+                alert('An error occurred while processing API calls');
+                console.log("Strokes csv call Failed: ", e);
+            },
+        
+            success: function (data) {
+                
+                if(data==undefined){
+                    console.log(data);
+                }
+        
+                var jsonData1 = $.csv.toObjects(data);
+        
+                console.log(jsonData1);
+                var b=-1;
+        
+                $.each(jsonData1, function (index, value) {
+                    b++;
+                    style.push(value["Style"]);
+                    var q=2;
+                    var temp_beat_cycle =[];
+                    var temp_speed =[];
+                    while(q<value["Beatcycle"].length){
+                        var flag=1;
+                        count =0;
+                        while(flag==1){
+                            flag=0;
+                            count++;
+                            var tempcyc ="";
+                            var temp_beat =[];
+                            for(;value["Beatcycle"][q]!=']';q++){
+                                if(value["Beatcycle"][q]== ' '){
+                                    flag=1;
+                                    q++;
+                                    break;
+                                }
+                                tempcyc+= value["Beatcycle"][q];
+                            }
+                            temp_beat.push(strokes[tempcyc]);
+                            temp_beat_cycle.push(temp_beat);
+                        }
+                        
+                        for(y=1;y<=count;y++){
+                            temp_speed.push(count);
+                        }
+                       
+                        
+                        q=q+2;
+                    }
+                    beatCyclet.push(temp_speed);
+                    beatCycle.push(temp_beat_cycle);
+                    
+                    /*temp_beat_cycle =[];
+                    temp_speed =[];
+                    q=2;
+                    if(value["Transition Cycle"].length!=0){
+                        tranenabled.push(1);
+                        if(parseInt(value["Anchor Point"])+parseInt(value["Total No. Of beats in Transition"]<parseInt(value["Total Number of Beats"]))){
+                            for(w=0;w<parseInt(value["Anchor Point"])-1;w++){
+                                temp_beat_cycle.push(beatCycle[b][w]);
+
+                            }
+                            while(q<value["Transition Cycle"].length){
+                                var flag=1;
+                                count =0;
+                                while(flag==1){
+                                    flag=0;
+                                    count++;
+                                    var tempcyc ="";
+                                    var temp_beat =[];
+                                    for(;value["Transition Cycle"][q]!=']';q++){
+                                        if(value["Transition Cycle"][q]== ' '){
+                                            flag=1;
+                                            q++;
+                                            break;
+                                        }
+                                        tempcyc+= value["Transition Cycle"][q];
+                                    }
+                                    temp_beat.push(strokes[tempcyc]);
+                                    temp_beat_cycle.push(temp_beat);
+                                }
+                                
+                                for(y=1;y<=count;y++){
+                                    temp_speed.push(count);
+                                }
+                                q=q+2;
+                            }
+                            for(w=parseInt(value["Anchor Point"])+parseInt(value["Total No. Of beats in Transition"]);w<value["Total Number of Beats"];w++){
+
+                            }
+
+                        }
+                    }
+                    else{
+                        tranenabled.push(0);
+                    }*/
+                    
+                });
+                console.log(beatCycle);
+                
+
+
+            }
+        });
+
+        
+    } // end: Ajax success API call
+
+}); // end: of Ajax call
+
 var dha_k = new Audio('assets/audio/dha_k.wav');
 var tin = new Audio('assets/audio/tin.wav');
 var tin_0 = new Audio('assets/audio/tin.wav');
@@ -5,6 +163,7 @@ var dha_m = new Audio('assets/audio/dha_m.wav');
 var dha_m0 = new Audio('assets/audio/dha_m.wav');
 var dha_m1 = new Audio('assets/audio/dha_m.wav');
 var ghe_hard = new Audio('assets/audio/ghe_hard.wav');
+var ghe_hard0 = new Audio('assets/audio/ghe_hard.wav');
 var ghe_soft = new Audio('assets/audio/ghe_soft.wav');
 var ghe_soft0 = new Audio('assets/audio/ghe_soft.wav');
 var khi = new Audio('assets/audio/khi.wav');
@@ -35,50 +194,45 @@ var blnk0 = new Audio('assets/audio/blnk.wav');
 var transcyc=[];
 
 
-var beatCycle1 = [[tin],[tin_0],[dha_k],[blnk]];
-var beatCycle1t = [1,1,1,1];
+//var beatCycle1 = [[tin],[tin_0],[dha_k],[blnk]];
+//var beatCycle1t = [1,1,1,1];
 
-var beatCycle2 = [[tin],[dha_k],[blnk]];
-var beatCycle2t = [1,1,1];
+//var beatCycle2 = [[tin],[dha_k],[blnk]];
+//var beatCycle2t = [1,1,1];
 
-var beatCycle3 = [[khi],[blnk],[khi0],[na],[dha_m],[ghe_soft],[dha_m0],[ghe_soft0],[dha_m1],[ghe_hard],[blnk],[blnk0],[na0],[te],[te0],[re],[te1],[re0],[na1]];
-var beatCycle3t = [1,2,2,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1];
+//var beatCycle3 = [[khi],[blnk],[khi0],[na],[dha_m],[ghe_soft],[dha_m0],[ghe_soft0],[dha_m1],[ghe_hard],[blnk],[blnk0],[na0],[te],[te0],[re],[te1],[re0],[na1]];
+//var beatCycle3t = [1,2,2,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1];
 var tran3=[[te],[re],[khi],[ta],[dha_m],[te0],[re0],[khi0],[ta0],[dha_m0],[te1],[re1],[khi1],[ta1],[dha_m1],[blnk],[blnk0],[na],[te2],[te3],[re2],[te4],[re3],[na0]];
 var tran3a= [2,2,2,2,1,2,2,2,2,1,2,2,2,2,1,1,1,1,1,2,2,2,2,1];
 
 
-var beatCycle4 = [[ghe_hard],[blnk],[na],[pa],[blnk],[ghe_hard],[dha_m],[blnk]];
-var beatCycle4t = [1,1,1,1,1,1,1,1];
+//var beatCycle4 = [[ghe_hard],[blnk],[na],[pa],[blnk],[ghe_hard],[dha_m],[blnk]];
+//var beatCycle4t = [1,1,1,1,1,1,1,1];
 
 var trana=[];
 var tranat=[];
 
-trana.push(beatCycle1);
-tranat.push(beatCycle1t);
-trana.push(beatCycle2);
-tranat.push(beatCycle2t);
+trana.push(beatCycle[0]);
+tranat.push(beatCyclet[0]);
+trana.push(beatCycle[1]);
+tranat.push(beatCyclet[1]);
 trana.push(tran3);
 tranat.push(tran3a);
-trana.push(beatCycle4);
-tranat.push(beatCycle4t);
+trana.push(beatCycle[3]);
+tranat.push(beatCyclet[3]);
 
 console.log(trana);
 var cnt=[4,3,16,8];
 var mx=7;
 var temp=0;
 
-var beatCycle = [];
-beatCycle.push(beatCycle1);
-beatCycle.push(beatCycle2);
-beatCycle.push(beatCycle3);
-beatCycle.push(beatCycle4);
-console.log(beatCycle);
 
-var beatCyclet=[];
+
+/*var beatCyclet=[];
 beatCyclet.push(beatCycle1t);
 beatCyclet.push(beatCycle2t);
 beatCyclet.push(beatCycle3t);
-beatCyclet.push(beatCycle4t);
+beatCyclet.push(beatCycle4t);*/
 
 var x=0;
 var bpm = 100;
@@ -87,6 +241,7 @@ var bpm_velocity=0;
 var flag =0;
 var tapbut=0;
 var tapcyc=0;
+var tapcyc_0=0;
 var tp=0;
 
 var audio;
@@ -96,41 +251,8 @@ var playlistt=[];
 var beat=[];
 var i =0;
 
-var dict={}
 
-var dict={}
-dict[dha_k]=0.026757369614512472;
-dict[tin]=0.005759637188208617;
-dict[tin_0]=0.005759637188208617;
-dict[dha_m]=0.07641723356009071;
-dict[dha_m0]=0.07641723356009071;
-dict[dha_m1]=0.07641723356009071;
-dict[ghe_hard]=0.03891156462585034;
-dict[ghe_soft]=0.028820861678004534;
-dict[ghe_soft0]=0.028820861678004534;
-dict[khi]=0.014331065759637189;
-dict[khi0]=0.014331065759637189;
-dict[khi1]=0.014331065759637189;
-dict[na]=0.04793650793650794;
-dict[na0]=0.04793650793650794;
-dict[na1]=0.04793650793650794;
-dict[ta]=0.035986394557823126;
-dict[ta0]=0.035986394557823126;
-dict[ta1]=0.035986394557823126;
-dict[re]=0.030702947845804986;
-dict[re0]=0.030702947845804986;
-dict[re1]=0.030702947845804986;
-dict[re2]=0.030702947845804986;
-dict[re3]=0.030702947845804986;
-dict[te]=0.03346938775510204;
-dict[te0]=0.03346938775510204;
-dict[te1]=0.03346938775510204;
-dict[te2]=0.03346938775510204;
-dict[te3]=0.03346938775510204;
-dict[te4]=0.03346938775510204;
-dict[blnk]=0.000;
-dict[blnk0]=0.000;
-dict[pa]=0.04793650793650794;
+
 
 function bpmvel(){
     bpm_velocity = parseInt(document.getElementById("bpm_velo").value);
@@ -159,6 +281,7 @@ function stylechange(){
         taped=0;
         tapbut=0;
         tapcyc=0;
+        tapcyc_0=0;
         document.getElementById("tap").value ="No Transition";
     }
    
@@ -227,10 +350,10 @@ function addcycle(){
     else if(x==2)document.getElementById("visual_style").innerHTML="The Current Playing style is Mridanga (16)" ;
     else if(x==3)document.getElementById("visual_style").innerHTML="The Current Playing style is Mridanga  (8)" ;
     var i =0;
-    if(tapbut==1){
+   /* if(tapbut==1){
         tapcyc++;
         document.getElementById("tap").value ="Mark Transition 2 at beatcycle " + tapcyc; 
-    }
+    }*/
     console.log("tp"+ tp);
     if(tapbut==2){
         tp++;
@@ -279,7 +402,11 @@ function playing(){
         addcycle();
         
     }
-    
+    if(tapbut==1){
+         tapcyc_0 += 1.0/cnt[x];
+         tapcyc = Math.round(tapcyc_0);
+        document.getElementById("tap").value ="Mark Transition 2 at beatcycle " + tapcyc; 
+    }
     beat=playlist[0];
     console.log(playlist[0]);
     k=playlistt[0];
@@ -317,10 +444,10 @@ function loop(){
     document.getElementById("bpm").innerHTML= bpm + " BPM";
     console.log(bpm);
     if(playlist.length>0){
-        console.log(beat[0]);
+        console.log("beat"+beat[0]);
         console.log(playlist[0][0]);
     }
-
+    
     
     if(playlist.length>0) t = setInterval(playing,60.0*1000/(bpmcurr)+1000*dict[beat[0]]-1000*dict[playlist[0][0]]);
     else t = setInterval(playing,60.0*1000/(bpmcurr)-1000*dict[beat[0]]+1000*dict[beatCycle[x][0][0]]);
@@ -341,6 +468,7 @@ function reset(){
     taped=0;
     tapbut=0;
     tapcyc=0;
+    tapcyc_0=0;
     
 
 }
@@ -358,39 +486,127 @@ function tap(){
     
 
 }
- var nooffav =0;
- var fav_style ="";
+var nooffav =0;
+var fav_style ="";
+var favarr =[];
 function addtofav(){
-    nooffav++;
-    var new_element = document.createElement("div");
-    new_element.setAttribute('id',"fav"+nooffav)
-    new_element.setAttribute('class',"col-12")
-    new_element.setAttribute('style',"padding-top:10px;")
-    console.log(x);
-    if(x==0)fav_style ="Kartaal (4)";
-    else if(x==1)fav_style ="Kartaal (3)";
-    else if(x==2)fav_style ="Mridanga (16)";
-    else if(x==3)fav_style ="Mridanga (8)";
-    new_element.innerHTML =  "<div class=container><div class=row><div class=col-5>"+fav_style+"</div><div class=col-2>"+ bpm +"</div><div class=col-3><button onclick= fav_play("+ bpm +','+ x +','+bpm_velocity+','+tapcyc+','+ tapbut +") style=color:#AC9B9B;background:rgba(2,15,40,0.64);border-radius:15px;> Play </button></div><div class=col-2><button onclick= fav_delete(fav"+nooffav+") style =color:#AC9B9B;background:rgba(2,15,40,0.64);border-radius:15px;> Delete </button></div></div></div>" ;
-    document.getElementById("fav").appendChild(new_element);
-    
-}
+        nooffav++;
+        var new_element = document.createElement("div");
+        new_element.setAttribute('id',"fav"+nooffav)
+        new_element.setAttribute('class',"col-12")
+        new_element.setAttribute('style',"padding-top:10px;")
+        console.log(x);
+        if(x==0)fav_style ="Kartaal (4)";
+        else if(x==1)fav_style ="Kartaal (3)";
+        else if(x==2)fav_style ="Mridanga (16)";
+        else if(x==3)fav_style ="Mridanga (8)";
+        var temparr = ["fav"+nooffav,x, bpm,bpm_velocity, tapbut, tapcyc];
+        favarr.push(temparr);
+        temparr =[];
+        new_element.innerHTML =  "<div class=container><div class=row><div class=col-5>"+fav_style+"</div><div class=col-2>"+ bpm +"</div><div class=col-3><button onclick= fav_play("+ bpm +','+ x +','+bpm_velocity+','+tapcyc+','+ tapbut +") style=color:#AC9B9B;background:rgba(2,15,40,0.64);border-radius:15px;> Play </button></div><div class=col-2><button onclick= fav_delete(fav"+nooffav+") style =color:#AC9B9B;background:rgba(2,15,40,0.64);border-radius:15px;> Delete </button></div></div></div>" ;
+        document.getElementById("fav").appendChild(new_element);
+        savefav();
+        
+    }
+
+    function savefav(){
+        document.getElementById("savefav").innerHTML = favarr;
+    }
+
+    function showfav(){
+        var a = document.getElementById("showfav").value;
+        console.log(a.length);
+        
+        z=0;
+        while(z<a.length){
+            nooffav++;
+            var new_element = document.createElement("div");
+            new_element.setAttribute('id',"fav"+nooffav)
+            new_element.setAttribute('class',"col-12")
+            new_element.setAttribute('style',"padding-top:10px;")
+            fav_save_x_s ="";
+            fav_save_bpm_s = "";
+            fav_save_bpm_velocity_s ="";
+            fav_save_tapbut_s="";
+            fav_save_tapcyc_s="";
+            for(;a[z]!=',';z++){
+
+            }
+            z++;
+            for(;a[z]!=',';z++){
+                fav_save_x_s+=a[z];
+            }
+            fav_save_x =parseInt(fav_save_x_s);
+            console.log(fav_save_x);
+            z++;
+            for(;a[z]!=',';z++){
+                fav_save_bpm_s+=a[z];
+            }
+            fav_save_bpm = parseInt(fav_save_bpm_s);
+            console.log(fav_save_bpm);
+            z++;
+            for(;a[z]!=',';z++){
+                fav_save_bpm_velocity_s +=a[z];
+            }
+            fav_save_bpm_velocity =parseInt(fav_save_bpm_velocity_s);
+            console.log(fav_save_bpm_velocity);
+            z++;
+            for(;a[z]!=',';z++){
+                fav_save_tapbut_s +=a[z];
+            }
+            fav_save_tapbut = parseInt(fav_save_tapbut_s);
+            console.log(fav_save_tapbut);
+            z++;
+            for(;a[z]!=','&&z!=a.length;z++){
+                
+                fav_save_tapcyc_s +=a[z];
+            }
+            fav_save_tapcyc =parseInt(fav_save_tapcyc_s);
+            console.log(fav_save_tapcyc);
+            z++;
+            if(fav_save_x==0)fav_save_style ="Kartaal (4)";
+            else if(fav_save_x==1)fav_save_style ="Kartaal (3)";
+            else if(fav_save_x==2)fav_save_style ="Mridanga (16)";
+            else if(fav_save_x==3)fav_save_style ="Mridanga (8)";
+            var temparr = ["fav"+nooffav,fav_save_x, fav_save_bpm,fav_save_bpm_velocity, fav_save_tapbut, fav_save_tapcyc];
+            favarr.push(temparr);
+            temparr =[];
+            new_element.innerHTML =  "<div class=container><div class=row><div class=col-5>"+fav_save_style+"</div><div class=col-2>"+ fav_save_bpm +"</div><div class=col-3><button onclick= fav_play("+ fav_save_bpm +','+ fav_save_x +','+fav_save_bpm_velocity+','+fav_save_tapcyc+','+ fav_save_tapbut +") style=color:#AC9B9B;background:rgba(2,15,40,0.64);border-radius:15px;> Play </button></div><div class=col-2><button onclick= fav_delete(fav"+nooffav+") style =color:#AC9B9B;background:rgba(2,15,40,0.64);border-radius:15px;> Delete </button></div></div></div>" ;
+            document.getElementById("fav").appendChild(new_element);
+            savefav();
+        }
+        
 
 
-function fav_delete(fav_number){
-    fav_number.remove();
-}
-function fav_play(fav_bpm,temp_style,fav_bpm_velocity ,fav_tapcyc,fav_tapbut){
-    
-    temp_bpm = fav_bpm;
-    temp_bpm_velocity = fav_bpm_velocity;
-    temp_tapcyc = fav_tapcyc;
-    temp_tapbut = fav_tapbut;
-    
-    x = temp_style;
-    
-    flag1 =1
-} 
 
+    }
 
+    function fav_delete(fav_number){
+        console.log(fav_number.id);
+        console.log(favarr[0]);
+        for(d=0;d<favarr.length;d++){
+            if(favarr[d].indexOf(fav_number.id)==0){
+                favarr.splice(d,1);
+                break;
+            }
+        }
+        savefav();
 
+        fav_number.remove();
+    }
+    function fav_play(fav_bpm,temp_style,fav_bpm_velocity ,fav_tapcyc,fav_tapbut){
+        
+        temp_bpm = fav_bpm;
+        temp_bpm_velocity = fav_bpm_velocity;
+        temp_tapcyc = fav_tapcyc;
+        temp_tapbut = fav_tapbut;
+        
+        x = temp_style;
+        
+        flag1 =1
+    }  
+
+           
+            
+       
+            
